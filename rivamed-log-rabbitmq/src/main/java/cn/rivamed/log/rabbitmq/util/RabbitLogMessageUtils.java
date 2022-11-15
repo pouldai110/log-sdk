c5e9650ee49da6af881614a143880216a3501689
zuojianhong <zuojianhong@rivamed.cn>
2022-11-15 10:45:25 +0800

    增加监听Quartz定时任务~

--- rivamed-log-rabbitmq/src/main/java/cn/rivamed/log/rabbitmq/util/RabbitLogMessageUtils.java
+++ rivamed-log-rabbitmq/src/main/java/cn/rivamed/log/rabbitmq/util/RabbitLogMessageUtils.java
@@ -2,16 +2,20 @@ package cn.rivamed.log.rabbitmq.util;
 
 import cn.rivamed.log.core.constant.LogMessageConstant;
 import cn.rivamed.log.core.entity.RabbitLogMessage;
+import cn.rivamed.log.core.util.JsonUtil;
 import cn.rivamed.log.rabbitmq.instrument.RabbitMQInstrumentation;
 import com.rabbitmq.client.Channel;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.connection.PublisherCallbackChannel;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 /**
  * @author Zuo Yang
@@ -61,7 +65,7 @@ public class RabbitLogMessageUtils {
      * @date 2022/10/17 9:58
      */
     public static RabbitLogMessage collectFromReceive(Message message, Channel channel) {
-        Object msg = null;
+        String msg = null;
         String host = "127.0.0.1", vhost = "/", exchange = null, routingKey = null, queue = null, messageId = null, className = null, methodName = null;
         Integer port = 6379;
         if (message != null) {
@@ -70,7 +74,8 @@ public class RabbitLogMessageUtils {
             exchange = messageProperties.getReceivedExchange();
             routingKey = messageProperties.getReceivedRoutingKey();
             queue = messageProperties.getConsumerQueue();
-            messageId = messageProperties.getMessageId();
+            //获取回调消息ID
+            messageId = messageProperties.getHeader(PublisherCallbackChannel.RETURNED_MESSAGE_CORRELATION_KEY);
             host = channel.getConnection().getAddress().getHostAddress();
             port = channel.getConnection().getPort();
             //从channel里面拿virtualHost,如果长度超过3，取最后一个
@@ -81,6 +86,10 @@ public class RabbitLogMessageUtils {
             //获取消息
             try {
                 msg = new String(message.getBody(), StandardCharsets.UTF_8.name());
+                if (StringUtils.isBlank(messageId)) {
+                    Map<String, String> msgMap = JsonUtil.json2Map(msg, String.class, String.class);
+                    messageId = msgMap.get("messageId");
+                }
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
---
+++ rivamed-log-task/src/main/java/cn/rivamed/log/task/util/TaskLogUtil.java
@@ -0,0 +1,17 @@
+package cn.rivamed.log.task.util;
+
+/**
+ * 描述: TODO
+ * 公司 北京瑞华康源科技有限公司
+ * 版本 Rivamed 2022
+ *
+ * @author 左健宏
+ * @version V2.0.1
+ * @date 22/11/14 10:33
+ */
+public class TaskLogUtil {
+
+    public static final String taskNormalFormatStr = " 在 %s 自动执行了 %s 方法，耗时%s 毫秒";
+    public static final String taskErrorFormatStr = " 在 %s 自动执行 %s 方法失败";
+
+}
