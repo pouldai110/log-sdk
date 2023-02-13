5323825ad2dccf9c2a856f063ecfe659f0034a05
zuojianhong <zuojianhong@rivamed.cn>
2023-02-13 15:25:41 +0800

    增加默认扫描防止客户端注册失败~

--- rivamed-log-spring-boot/rivamed-log-spring-boot-configuration/src/main/java/cn/rivamed/log/springboot/configuration/RivamedLogCommonAutoConfiguration.java
+++ rivamed-log-spring-boot/rivamed-log-spring-boot-configuration/src/main/java/cn/rivamed/log/springboot/configuration/RivamedLogCommonAutoConfiguration.java
@@ -3,9 +3,11 @@ package cn.rivamed.log.springboot.configuration;
 import cn.rivamed.log.core.spring.RivamedLogApplicationContextHolder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("cn.rivamed.log")
 public class RivamedLogCommonAutoConfiguration {
 
     @Bean
